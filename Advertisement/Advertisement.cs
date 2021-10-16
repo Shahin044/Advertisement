7f23f231766f79d7bb2b6281d7a0f12820ffd504
Shahin044 <Shahinrm@code.edu.az>
2021-10-16 09:48:34 +0400

    Advertisement

---
+++ Advertisement/Advertisement.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advertisement
+{
+    class Advertisement
+    {
+        public string Name { get; set; }
+
+
+        public int Price { get; set; }
+
+        public string NameSeller { get; set; }
+
+
+        public Advertisement(string name, int price, string nameSeller)
+        {
+            Name = name;
+            Price = price;
+            NameSeller = nameSeller;
+
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} {Price} {NameSeller} ";
+
+        }
+
+        public int CompareTo(object obj)
+        {
+            Advertisement advertisementToCompare = (Advertisement)obj;
+            if (Price > advertisementToCompare.Price)
+            {
+                return 1;
+            }
+            else if (Price == advertisementToCompare.Price)
+            {
+                return 0;
+            }
+            else
+            {
+                return -1;
+            }
+
+
+        }
+        public static bool operator >(Advertisement a1, Advertisement a2)
+        {
+            return a1.Price > a2.Price;
+        }
+
+        public static bool operator <(Advertisement a1, Advertisement a2)
+        {
+            return a1.Price < a2.Price;
+        }
+
+    }
+}
---
+++ Advertisement/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advertisement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Advertisement a1 = new Advertisement("game", 500, "Əli");
+            Advertisement a2 = new Advertisement("computer", 200, "Vaqif");
+            Advertisement a3 = new Advertisement("mobile", 400, "Seymur");
+
+            Advertisement[] price = { a1, a2, a3 };
+
+            try
+            {
+                Array.Sort(price);
+            }
+            catch (Exception)
+            {
+
+                Console.WriteLine("Gonderdiyin arry sort oluna bilmir");
+            }
+
+            foreach (Advertisement item in price)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine(a1 > a2);
+
+
+        }
+    }
+    enum Category : byte
+    {
+        Radio, Mobile, Social
+    }
+    }
